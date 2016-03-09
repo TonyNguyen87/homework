c77cc166eb45cfd6855ee83a560b055367b6f2c3
Tony Nguyen <tonyviet87@gmail.com>
2016-03-09 08:50:29 -0500

    couldnt get close_issue and comment_issue to work.

--- lib/homework.rb
+++ lib/homework.rb
@@ -1,13 +1,23 @@
 require "pry"
 require "httparty"
-
-require "homework/version"
-require "homework/github"
+require "json"
+# require "./version"
+require "./homework/github"
 
 module Homework
   # Your code goes here...
 end
 
+# "/repos/TIY-ATL-ROR-2016-Feb/assignments/issues"
+
 github = Homework::Github.new
 
 binding.pry
+
+## create an issue in /TIY-ATL-ROR-2016-Feb/homework
+## to close issue
+
+#kingcons, coleslaw, cl-6502
+#owner,      repo
+#rails, rails,
+#owner, repo
--- lib/homework/github.rb
+++ lib/homework/github.rb
@@ -1,10 +1,12 @@
+require "json"
+
 module Homework
   class Github
     include HTTParty
     base_uri "https://api.github.com"
 
     def initialize
-      @auth_token = "a8ba164f9526540607f9a91b0625120ff5352f3e"
+      @auth_token = "7097428a0d4bf47b77e49e99575e61107b678fcf"
       @headers = {
         "Authorization" => "token #{@auth_token}",
         "User-Agent"    => "HTTParty"
@@ -28,5 +30,18 @@ module Homework
     def list_team_members(team_id)
       Github.get("/teams/#{team_id}/members", headers: @headers)
     end
+
+    def list_issues(owner, repo) #rails organization has rail issue, kingcons, coleslaw, cl-6502, rails, rails,
+      Github.get("/repos/#{owner}/#{repo}/issues", headers: @headers)
+    end
+ 
+    def close_issue(owner, repo, issue_num)
+      Github.patch("/repos/#{owner}/#{repo}/issues/#{issue_num}", headers: @headers, :state => { option: "closed" }.to_json)
+    end
+
+    def comment_issue(owner, repo, issue_num, comment)
+      Github.patch("/repos/#{owner}/#{repo}/issues/#{issue_num}/comments", headers: @headers, :body => { option: "#{comment}" }.to_json)
+    end
   end
 end
+
